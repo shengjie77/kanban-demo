859fb286490bc50e9510c5335c1a86543bc204ed
Sheng Jie <shengjie0202@outlook.com>
2021-07-31 13:12:02 +0800

    feat: Init SwiftUI

---
+++ swift-ui/Kanban/KanbanApp.swift
@@ -0,0 +1,17 @@
+//
+//  KanbanApp.swift
+//  Kanban
+//
+//  Created by Sheng Jie on 2021/7/31.
+//
+
+import SwiftUI
+
+@main
+struct KanbanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
